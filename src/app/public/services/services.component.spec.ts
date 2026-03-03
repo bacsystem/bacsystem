96f08a49d305ad6a7c7559ba4ffad5653765bc75
cbacilio <cbacilio@novopayment.com>
2026-03-03 10:35:01 -0500

    feat: github actions

--- src/app/public/services/services.component.spec.ts
+++ src/app/public/services/services.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { ServicesComponent } from './services.component';
 
@@ -8,7 +10,7 @@ describe('ServicesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ServicesComponent]
+      imports: [ServicesComponent, RouterTestingModule, TranslateModule.forRoot({})]
     })
     .compileComponents();
 
